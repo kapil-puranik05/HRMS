682c4f37cc5802ed26e0cc517ea89b9cb7b7d454
Kapil Puranik <kapilpuranik764@gmail.com>
2025-11-21 10:05:17 +0530

    Disabled, Cron test

--- cron/cmd/main.go
+++ cron/cmd/main.go
@@ -14,6 +14,6 @@ func main() {
 	//Testing the cron setup with a cron job that runs every minute,
 	//performing the same task as the DispatchReports() Method
 
-	go services.CronTest() //Remove the comment from this method to test the working of weekly and monthly report generation(Every 10s)
+	//go services.CronTest() //Remove the comment from this method to test the working of weekly and monthly report generation(Every 10s)
 	select {}
 }
